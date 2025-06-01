8795a5401038a3b0c9babeb0cf8377bb7a86754e
7odaco <m.h.adbalqader@students.ptuk.edu.ps>
2025-06-01 17:07:52 +0300

    Add files via upload

---
+++ Appdata.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 7odaco
+ */
+public class Appdata {
+    public static int creditscore=0;
+    public static int score=0;
+    
+}
